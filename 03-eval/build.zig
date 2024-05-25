d48fb68f44389ec6563e0d759bca579dc885e6df
Jérôme MOUREY <jerome@everycheck.fr>
2024-05-25 23:29:08 +0200

    clean build

--- 03-eval/build.zig
+++ 03-eval/build.zig
@@ -1,28 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+	const exe_name = b.option(
+		[]const u8,
+		"exe_name",
+		"Name of the executable",
+	) orelse "eval";
 
 	const exe = b.addExecutable(.{
-		.name = "eval",
+		.name = exe_name,
 		.root_source_file = b.path("src/main.zig"),
-		.target = b.host,
+		.target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
 	});
-
-    // const srcDir = "src";
-    // addSourceFiles(b, exe, srcDir) catch {
-    //     std.debug.warn("Erreur lors de l'ajout des fichiers sources: {}\n", .{srcDir});
-    //     return;
-    // };
     b.installArtifact(exe);
-}
 
-// fn addSourceFiles(b: *std.Build, exe: *std.Build.Step.Compile, path: []const u8) !void {
-// 	var dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
-// 	var walker = try dir.walk(b.allocator);
-// 	defer walker.deinit();
-// 	while (try walker.next()) |entry| {
-// 		if (entry.kind == .file and std.mem.endsWith(u8, entry.path, ".zig")) {
-// 			exe.addPackagePath(entry.name, entry.path);
-// 		}
-// 	}
-// }
+    const run_exe = b.addRunArtifact(exe);
+
+	const run_step = b.step("run", "Run the application");
+	run_step.dependOn(&run_exe.step);
+}
