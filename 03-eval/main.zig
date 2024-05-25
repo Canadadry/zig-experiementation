d409fcd0676bda04cb4a58abb6a1e0676993c385
Jérôme MOUREY <jerome@everycheck.fr>
2024-05-25 22:48:07 +0200

    can run parser and catch EXC_BAD_ACCESS error

---
+++ 03-eval/main.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const lexer = @import("lexer.zig");
+const parser = @import("parser.zig");
+
+
+pub fn main() !void{
+	run() catch |err|{
+		std.debug.print("failed {}\n",.{err});
+		std.process.exit(1);
+	};
+}
+
+fn run() !void{
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+	defer _ = gpa.deinit();
+	const alloc = gpa.allocator();
+	const args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
+
+	if(args.len!=2){
+		std.debug.print("expect 1 argument\n",.{});
+		return error.missingCliArguments;
+	}
+
+	var l = lexer.Lexer.init(args[1]);
+	var p = parser.Parser.init(&l);
+ 	const node = try p.parseExpression(.lowest);
+  	std.debug.print("{}\n",.{node.eval()});
+}
