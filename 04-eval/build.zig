0ae20d9e2a64df5a73df80e4b319254e32081a99
Canadadry <moureyjerome-spam@yahoo.fr>
2026-05-27 09:53:14 +0200

    update build and main to 0.16

--- 04-eval/build.zig
+++ 04-eval/build.zig
@@ -1,22 +1,35 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-	const exe_name = b.option(
-		[]const u8,
-		"exe_name",
-		"Name of the executable",
-	) orelse "eval";
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-	const exe = b.addExecutable(.{
-		.name = exe_name,
-		.root_source_file = b.path("src/main.zig"),
-		.target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
-	});
+    const exe = b.addExecutable(.{
+        .name = "_04_eval",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{},
+        }),
+    });
+
     b.installArtifact(exe);
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
 
-    const run_exe = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
-	const run_step = b.step("run", "Run the application");
-	run_step.dependOn(&run_exe.step);
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
 }
--- 04-eval/src/main.zig
+++ 04-eval/src/main.zig
@@ -2,29 +2,24 @@ const std = @import("std");
 const lexer = @import("lexer.zig");
 const parser = @import("parser.zig");
 
-
-pub fn main() !void{
-	run() catch |err|{
-		std.debug.print("failed {}\n",.{err});
-		std.process.exit(1);
-	};
+pub fn main(init: std.process.Init) !void {
+    run(init) catch |err| {
+        std.debug.print("failed {}\n", .{err});
+        std.process.exit(1);
+    };
 }
 
-fn run() !void{
-	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-	defer _ = gpa.deinit();
-	const alloc = gpa.allocator();
-	const args = try std.process.argsAlloc(alloc);
-    defer std.process.argsFree(alloc, args);
+fn run(init: std.process.Init) !void {
+    const alloc = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(alloc);
+    if (args.len != 2) {
+        std.debug.print("expect 1 argument\n", .{});
+        return error.missingCliArguments;
+    }
 
-	if(args.len!=2){
-		std.debug.print("expect 1 argument\n",.{});
-		return error.missingCliArguments;
-	}
-
-	var l = lexer.Lexer.init(args[1]);
-	var p = try parser.Parser.init(alloc,&l);
-	defer p.deinit();
- 	const node = try p.parse();
-  	std.debug.print("{}\n",.{node.eval()});
+    var l = lexer.Lexer.init(args[1]);
+    var p = try parser.Parser.init(alloc, &l);
+    defer p.deinit();
+    const node = try p.parse();
+    std.debug.print("{}\n", .{node.eval()});
 }
