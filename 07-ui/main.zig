ea55db7c3444be43f12545fd7c29cdb2ee1e6180
Canadadry <moureyjerome-spam@yahoo.fr>
2026-06-13 14:47:46 +0200

    allow multi painter per node

--- 07-ui/main.zig
+++ 07-ui/main.zig
@@ -73,6 +73,57 @@ pub const Painter = union(enum) {
     }
 };
 
+pub const MultiPainter = struct {
+    const max_painters = 8;
+    count: usize = 0,
+    items: [max_painters]Painter = undefined,
+
+    pub fn from(comptime len: usize, painters: [len]Painter) @This() {
+        comptime {
+            if (len > max_painters) {
+                @compileError("max painter on one node is " ++ max_painters);
+            }
+        }
+        var mp: @This() = .{ .count = len };
+        @memcpy(mp.items[0..len], &painters);
+        return mp;
+    }
+
+    pub fn measure_content_fn(self: *@This()) [2]i32 {
+        var max_size: [2]i32 = .{ 0, 0 };
+        for (0..self.count) |i| {
+            var p = self.items[i];
+            const current = p.measure_content_fn();
+            if (max_size[0] < current[0]) {
+                max_size[0] = current[0];
+            }
+            if (max_size[1] < current[1]) {
+                max_size[1] = current[1];
+            }
+        }
+        return max_size;
+    }
+
+    pub fn wrap_content_fn(self: *@This(), width: i32) i32 {
+        var max_height: i32 = 0;
+        for (0..self.count) |i| {
+            var p = self.items[i];
+            const current = p.wrap_content_fn(width);
+            if (max_height < current) {
+                max_height = current;
+            }
+        }
+        return max_height;
+    }
+
+    pub fn draw(self: *@This(), x: u32, y: u32, w: u32, h: u32) void {
+        for (0..self.count) |i| {
+            var p = self.items[i];
+            p.draw(x, y, w, h);
+        }
+    }
+};
+
 pub fn None() Painter {
     return Painter{ .none = .{} };
 }
@@ -98,8 +149,8 @@ pub fn Txt(txt: [:0]const u8) Painter {
                 .spacing = 2,
                 .familly = rl.GetFontDefault(),
                 .@"align" = .{
-                    .x = .begin,
-                    .y = .begin,
+                    .x = .middle,
+                    .y = .middle,
                 },
             },
         },
@@ -116,24 +167,24 @@ pub fn main() !void {
     defer rl.CloseWindow();
     rl.SetTargetFPS(60);
 
-    const uib = zui.Builder.Builder(Painter, Painter{ .none = .{} });
-
-    const root = uib.node("col gap-10 p-10 w-400 h-300", Rect(rl.RED), &.{
-        uib.node("row gap-10 grow-x", Rect(rl.GREEN), &.{
-            uib.leaf("w-120 h-50", Txt("btn-a")),
-            uib.leaf("w-120 h-50", Txt("btn-b")),
+    const uib = zui.Builder.Builder(MultiPainter, .{});
+    const from = MultiPainter.from;
+    const root = uib.node("col gap-10 p-10 w-400 h-300", from(1, .{Rect(rl.RED)}), &.{
+        uib.node("row gap-10 grow-x", from(1, .{Rect(rl.GREEN)}), &.{
+            uib.leaf("w-120 h-50", from(2, .{ Rect(rl.YELLOW), Txt("btn-a") })),
+            uib.leaf("w-120 h-50", from(2, .{ Rect(rl.YELLOW), Txt("btn-b") })),
         }),
-        uib.node("row gap-10 grow-x", Rect(rl.BLUE), &.{
-            uib.leaf("grow h-200", Txt("canvas")),
-            uib.leaf("w-150 h-200", Txt("sidebar")),
+        uib.node("row gap-10 grow-x", from(1, .{Rect(rl.BLUE)}), &.{
+            uib.leaf("grow h-200", from(2, .{ Rect(rl.YELLOW), Txt("canvas") })),
+            uib.leaf("w-150 h-200", from(2, .{ Rect(rl.YELLOW), Txt("sidebar") })),
         }),
     });
 
-    var list = std.array_list.Managed(zui.Ui.Node(Painter, Painter{ .none = .{} })).init(alloc);
+    var list = std.array_list.Managed(zui.Ui.Node(MultiPainter, .{})).init(alloc);
     defer list.deinit();
     try uib.build(root, &list);
 
-    var tree: zui.Ui.Tree(Painter, .{ .none = .{} }) = .{};
+    var tree: zui.Ui.Tree(MultiPainter, .{}) = .{};
     tree.init(alloc);
     defer tree.deinit();
 
